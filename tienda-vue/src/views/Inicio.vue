661d1f02f12fa36d9777d52b9f4b6ce28b9dfb8d
Nelch3vez <88854087+Nelch3vez@users.noreply.github.com>
2022-04-26 09:42:16 -0600

    Update Inicio.vue

---
+++ tienda-vue/src/views/Inicio.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="inicio">
+        <Index/>
+    </div>
+    
+</template>
+
+<script>
+import Index from '@/components/inicio.vue'
+
+export default {
+    name:"inicio",
+    components: {
+        Index
+    }
+}
+</script>
