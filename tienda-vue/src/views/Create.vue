dd31fa86b14ceb17a2d57f367b5e156426286bfb
JonatanMoraga <88854031+JonatanMoraga@users.noreply.github.com>
2022-04-26 09:48:08 -0600

    Update Create.vue

---
+++ tienda-vue/src/views/Create.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="create">
+        <Crear/>
+    </div>
+    
+</template>
+
+<script>
+import Crear from '@/components/create.vue'
+
+export default {
+    name:"create",
+    components: {
+        Crear
+    }
+}
+</script>
