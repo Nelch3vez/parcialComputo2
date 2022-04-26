9af44746354bf372af7b2f353ba322554044ec49
ElianGuillen07 <98608696+ElianGuillen07@users.noreply.github.com>
2022-04-26 10:48:02 -0500

    Update Edit.vue

---
+++ tienda-vue/src/views/Edit.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="edit">
+        <Edit/>
+    </div>
+    
+</template>
+
+<script>
+
+import Edit from '@/components/edit.vue'
+
+export default {
+    name:"edit",
+    components: {
+
+        Edit
+    }
+}
+</script>
